cef72c1d300ff86a154b4e08ce557ff9a2f798e0
CubeCmder <65124077+CubeCmder@users.noreply.github.com>
2022-12-05 14:16:26 -0500

    push

--- PA/SerialGPS.py
+++ PA/SerialGPS.py
@@ -24,6 +24,7 @@ from gps import *
 from time import *
 import time
 import threading
+import plotext as plt;
 
 gpsd = None #seting the global variable
 
@@ -49,6 +50,8 @@ if __name__ == '__main__':
   avg_lat = 0
   avg_long = 0
   done = False
+  x = []
+  y = []
   try:
     gpsp.start() # start it up
     while True:
@@ -59,16 +62,19 @@ if __name__ == '__main__':
         avg_lat = 0
         avg_long = 0
         done = True
+        x = []
+        y = []
 
       os.system('clear')
-
+      lat = gpsd.fix.latitude
+      long = gpsd.fix.longitude
       print()
       print (' GPS reading')
       print ('----------------------------------------')
-      print ('latitude        ' , gpsd.fix.latitude)
-      print ('longitude       ' , gpsd.fix.longitude)
-      avg_lat = (avg_lat * N + gpsd.fix.latitude)/(N+1)
-      avg_long = (avg_long * N + gpsd.fix.longitude)/(N+1)
+      print ('latitude        ' , lat)
+      print ('longitude       ' , long)
+      avg_lat = (avg_lat * N + lat)/(N+1)
+      avg_long = (avg_long * N + long)/(N+1)
       print('average coords   ' , avg_lat,',',avg_long)
       print ('time utc        ' , gpsd.utc,' + ', gpsd.fix.time)
       print ('altitude (m)    ' , gpsd.fix.altitude)
@@ -82,6 +88,8 @@ if __name__ == '__main__':
       print ('mode            ' , gpsd.fix.mode)
       print()
       print('sats             ', gpsd.satellites)
+      y.append(lat)
+      x.append(long)
 
       time.sleep(1)  # set to whatever
       N+=1
@@ -90,4 +98,22 @@ if __name__ == '__main__':
     print("\nKilling Thread...")
     gpsp.running = False
     gpsp.join() # wait for the thread to finish what it's doing
+
+
+
+  l, p = avg_long, avg_lat;
+  plt.plot(x,y, marker = 'x');
+  #plt.plotsize(100, 30);
+  plt.title('Some Smart Title');
+  plt.xlabel('Longitude');
+  plt.ylabel('Latitude');
+  plt.ticks_color('red');
+  plt.ticks_style('bold');
+  #plt.xlim(-l // 10, l + l // 10);
+  #plt.ylim(-1.5, 1.5);
+  #xticks = [l * i / (2 * p) for i in range(2 * p + 1)];
+  #xlabels = [str(i) + 'π' for i in range(2 * p + 1)];
+  #plt.xticks(xticks, xlabels);
+  plt.show()
+
   print("Done.\nExiting.")
