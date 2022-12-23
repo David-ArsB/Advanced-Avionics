748f35f79ee1faaeb0473451664bc9ae1d74627d
CubeCmder <65124077+CubeCmder@users.noreply.github.com>
2022-12-22 22:01:03 -0500

    push

--- PA/SerialGPS2.py
+++ PA/SerialGPS2.py
@@ -38,5 +38,5 @@ print("Receiving GPS data")
 ser = serial.Serial(port, baudrate=9600, timeout=5)
 while True:
     data = ser.readline()
-    #print(data.decode('ascii', errors = 'replace').strip())
+    print(data.decode('ascii', errors = 'replace').strip())
     parseGPS(data)
