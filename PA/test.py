199c3a5881902279946a59a152d7da4f739fb686
CubeCmder <65124077+CubeCmder@users.noreply.github.com>
2022-12-22 22:35:37 -0500

    push

--- PA/test.py
+++ PA/test.py
@@ -4,14 +4,15 @@ import pynmea2
 port = "/dev/ttyS0"
 
 
-def parseGPS(str):
-    if str.find('GGA') > 0:
-        msg = pynmea2.parse(str)
+def parseGPS(buf):
+    if buf.find('GGA') > 0:
+        msg = pynmea2.parse(buf)
         print("Timestamp: %s -- Lat: %s %s -- Lon: %s %s -- Altitude: %s %s -- Satellites: %s" %
             (msg.timestamp, msg.lat, msg.lat_dir, msg.lon, msg.lon_dir, msg.altitude, msg.altitude_units, msg.num_sats))
 
 
 serialPort = serial.Serial(port, baudrate=9600, timeout=0.5)
 while True:
-    str = serialPort.readline().decode('ascii', errors = 'replace').strip()
-    parseGPS(str)
+    buf = serialPort.readline().decode('ascii', errors = 'replace').strip()
+    print(buf)
+    parseGPS(buf)
