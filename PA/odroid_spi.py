1cfe14725f09934f45fb6930e52c1286d39b08a3
CubeCmder <65124077+CubeCmder@users.noreply.github.com>
2022-12-30 11:15:04 -0500

    b

--- PA/odroid_spi.py
+++ PA/odroid_spi.py
@@ -1,7 +1,20 @@
+# import Odroid.GPIO as GPIO
+# import spidev
+# GPIO.setmode(GPIO.BCM)
+# spi = spidev.SpiDev()
+# spi.open(0, 0)
+# spi.max_speed_hz = 4000000
+# GPIO.setup(16, GPIO.OUT)
+
 import Odroid.GPIO as GPIO
-import spidev
+import time
+
+redLedGPIO=16
 GPIO.setmode(GPIO.BCM)
-spi = spidev.SpiDev()
-spi.open(0, 0)
-spi.max_speed_hz = 4000000
-GPIO.setup(16, GPIO.OUT)
+GPIO.setup(redLedGPIO,GPIO.OUT)
+
+while True:
+        GPIO.output(redLedGPIO,GPIO.HIGH)
+        time.sleep(1)
+        GPIO.output(redLedGPIO,GPIO.LOW)
+        time.sleep(1)
