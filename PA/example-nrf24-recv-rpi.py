59c0a4195b7d46f38506ec9d2d539d3de3052479
CubeCmder <65124077+CubeCmder@users.noreply.github.com>
2023-01-02 14:32:20 -0500

    g

--- PA/example-nrf24-recv-rpi.py
+++ PA/example-nrf24-recv-rpi.py
@@ -12,15 +12,15 @@ import spidev
 
 
 
-pipes = [[0xe7, 0xe7, 0xe7, 0xe7, 0xe7], [0xc2, 0xc2, 0xc2, 0xc2, 0xc2]]
+pipes = [[0xE0, 0xE0, 0xE0, 0xE0, 0xE0], [0xc2, 0xc2, 0xc2, 0xc2, 0xc2]]
 
 radio2 = NRF24(GPIO, spidev.SpiDev())
-radio2.begin(0, 17)
+radio2.begin(0, 25)
 
 radio2.setRetries(15,15)
 
 radio2.setPayloadSize(32)
-radio2.setChannel(0x60)
+radio2.setChannel(0x76)
 radio2.setDataRate(NRF24.BR_2MBPS)
 radio2.setPALevel(NRF24.PA_MIN)
 
@@ -28,7 +28,7 @@ radio2.setAutoAck(True)
 radio2.enableDynamicPayloads()
 radio2.enableAckPayload()
 
-radio2.openWritingPipe(pipes[1])
+#radio2.openWritingPipe(pipes[1])
 radio2.openReadingPipe(1, pipes[0])
 
 radio2.startListening()
