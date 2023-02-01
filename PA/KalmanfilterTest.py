5b3c26413fae5bd546c727ba0c4bb21eeeaff28d
Catattack120 <119343851+Catattack120@users.noreply.github.com>
2023-02-01 12:00:44 -0500

    Update KalmanfilterTest.py

--- PA/KalmanfilterTest.py
+++ PA/KalmanfilterTest.py
@@ -101,7 +101,7 @@ for i in range(1,m):
        # print(lat,lon)
 
         coords=np.append(coords,[lat,lon])
-        #print(coords)
+        print(coords)
         xy=get_xy(coords)
    else:
         mpx=np.append(mpx,mpx[i-1])
